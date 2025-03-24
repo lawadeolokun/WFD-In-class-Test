867da914264f2d8fdb9c84d7d769e7fd13599ae2
Lawrence <B00153847@mytudublin.ie>
2025-03-24 14:28:35 +0000

    A4

--- PartA.py
+++ PartA.py
@@ -6,9 +6,39 @@ class Staff:
         self.staffID = staffID
         self.address = address
 
-        def printInitAttributes(self):
+        def print_init_attributes(self):
             print(f"Name: {self.name}")
             print(f"DoB: {self.DoB}")
             print(f"Sex: {self.sex}")
             print(f"Staff ID: {self.staffID}")
             print(f"Address: {self.address}")
+
+        def update_name(self,new_name):
+            if isinstance(new_name, str):
+                self.name = new_name
+            else:
+                print("Name entered is invalid")
+            
+        def update_DoB(self,new_DoB):
+            if isinstance(new_DoB, str):
+                self.DoB = new_DoB
+            else:
+                print("DOB entered is invalid")
+
+        def update_sex(self,new_sex):
+            if isinstance(new_sex, str):
+                self.sex = new_sex
+            else:
+                print("Sex entered is invalid")
+
+        def update_staffID(self,new_staffID):
+            if isinstance(new_staffID, str):
+                self.staffID = new_staffID
+            else:
+                print("Staff ID entered is invalid")
+
+        def update_address(self, new_address):
+            if isinstance(new_address, str):
+                self.address = new_address
+            else:
+                print("Address entered is invalid")
